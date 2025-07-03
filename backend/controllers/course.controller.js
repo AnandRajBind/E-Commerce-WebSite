4b2550c1a8473ed52d8076c66b85ab5143674423
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-07-03 16:43:38 +0530

    03/07/2025
    Make  a update Course, Delete Course, getCourses, course Details API.

--- backend/controllers/course.controller.js
+++ backend/controllers/course.controller.js
@@ -12,13 +12,13 @@ export const createCourse = async (req, res) => {
   try {
     if (!title || !description || !price) {
       console.log("Step 3: Validation failed - Missing fields");
-      return res.status(400).json({ error: "All fields are required" });
+      return res.status(400).json({ errors: "All fields are required" });
     }
-        if (!req.files || !req.files.image) {
+    if (!req.files || !req.files.image) {
 
-    // if (!req.files || Object.keys(req.files).length === 0) {
+      // if (!req.files || Object.keys(req.files).length === 0) {
       console.log("Step 4: Image file missing", req.files);
-       return res.status(400).json({ error: "No files Uploaded" })
+      return res.status(400).json({ errors: "No files Uploaded" })
     }
 
     const { image } = req.files; // Extracting the image file from the request files. This assumes that the request is made with a file upload, typically using a library like `express-fileupload` or `multer`.
@@ -26,19 +26,19 @@ export const createCourse = async (req, res) => {
 
     const allowedFormat = ["image/png", "image/jpeg"];
     if (!allowedFormat.includes(image.mimetype)) {
-            console.log("Step 6: Invalid image format", image.mimetype);
+      console.log("Step 6: Invalid image format", image.mimetype);
 
-      return res.status(400).json({ error: "Invalid image format. Only jpg and png are allowed." });
+      return res.status(400).json({ errors: "Invalid image format. Only jpg and png are allowed." });
     }
-    
+
     // cloudinary upload code
     const cloud_response = await cloudinary.uploader.upload(image.tempFilePath);
-        console.log("Step 7: Cloudinary upload response", cloud_response);
+    console.log("Step 7: Cloudinary upload response", cloud_response);
 
     if (!cloud_response || cloud_response.error) {
-            console.log("Step 8: Cloudinary error");
+      console.log("Step 8: Cloudinary error");
 
-      return res.status(400).json({ error: "Error uploading image to Cloudinary" });
+      return res.status(400).json({ errors: "Error uploading image to Cloudinary" });
     }
     const courseData = {
       title,
@@ -49,20 +49,86 @@ export const createCourse = async (req, res) => {
         url: cloud_response.secure_url,
       },
     };
-        console.log("Step 9: Course data ready", courseData);
+    console.log("Step 9: Course data ready", courseData);
 
     const course = await Course.create(courseData);// Using the Course model to create a new course document in the database.
-        console.log("Step 10: Course created in DB", course);
+    console.log("Step 10: Course created in DB", course);
 
     res.json({
       message: "Course created successfully",
       course,
     });
   } catch (error) {
-        console.log("Step 11: Server error", error);
-    console.error(error);
-    res.status(500).json({ error: "Error creating course" });
+    console.log("Step 11: Server error", error); console.error(error);
+    res.status(500).json({ errors: "Error creating course" });
   }
-};
+}
 
+export const updateCourse = async (req, res) => {
+  const { courseId } = req.params; // Extracting the courseId from the request parameters
+  const { title, description, price, image } = req.body; // Destructuring the request body to get course details
+  try {
+    const course = await Course.updateOne({ _id: courseId },
+      {
+        title,
+        description,
+        price,
+        image: {
+          public_id: image.public_id,
+          url: image.url
+        }
+      }
+    )
+    res.status(201).json({
+      message: "Course updated successfully",
+    })
+  } catch (error) {
+    res.status(500).json({ error: "Error in course updating" });
+    console.log("Error in course updating", error);
+  }
+}
+export const deleteCourse = async (req, res) => {
+  const { courseId } = req.params;
+  try {
+    const course = await Course.findOneAndDelete({ _id: courseId })
+    if (!course) {
+      return res.status(404).json({ errors: "Course not found" })
+    }
+    res.status(200).json({
+      message: "Course deleted successfully",
+      course
+    })
+  }
+  catch (error) {
+    res.status(500).json({ errors: "Error in course deleting" });
+  }
+}
 
+export const getCourses = async (req, res) => {
+  try {
+    const courses = await Course.find({});
+    res.status(200).json({
+      message: "Courses fetched successfully",
+      courses
+    });
+  } catch (error) {
+
+    res.status(500).json({ errors: "Error fetching courses" });
+    console.log("Error fetching courses", error);
+  }
+}
+export const courseDetails = async (req, res) => {
+  const { courseId } = req.params;
+  try {
+    const course = await Course.findById(courseId)
+    if (!course) {
+      return res.status(404).json({ errors: "Course not found" });
+    }
+    res.status(500).json({ message: "Course details fetched successfully", course });
+
+  } catch (error) {
+    res.status(500).json({ errors: "Error fetching course details" });
+    console.log("Error fetching course details", error);
+  }
+
+}
--- backend/routes/course.route.js
+++ backend/routes/course.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createCourse } from '../controllers/course.controller.js';
+import { createCourse, updateCourse, deleteCourse, getCourses,courseDetails} from '../controllers/course.controller.js';
 
 const router = express.Router();// create express router
 
@@ -8,5 +8,8 @@ const router = express.Router();// create express router
 // The router is an instance of express.Router() that allows us to define routes for our application.
 // define route for creating a course
 router.post('/create', createCourse);
-
-export default router; 
+router.put('/update/:courseId', updateCourse);
+router.delete('/delete/:courseId', deleteCourse);
+router.get('/Courses', getCourses);
+router.get('/:courseId', courseDetails);
+export default router; // Export
