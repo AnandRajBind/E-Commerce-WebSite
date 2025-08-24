a9a0f214a28b30c871a4152abd54826a7eda5448
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-08-24 12:54:50 +0530

    24/08/2025
    Backend url set

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,2 +1,3 @@
 // export const BACKEND_URL = 'https://coursebase.onrender.com';
-export const BACKEND_URL = 'http://localhost:4001/api/v1';
+// export const BACKEND_URL = 'http://localhost:4001/api/v1';
+export const BACKEND_URL = 'https://e-commerce-website-27zc.onrender.com/';
